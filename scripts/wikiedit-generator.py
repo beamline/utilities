2d5de18933c36bca45be2d46c04dddfaf0040a29
Andrea Burattin <delas@users.noreply.github.com>
2021-10-15 16:22:02 +0200

    Add files via upload

---
+++ scripts/wikiedit-generator.py
@@ -0,0 +1,28 @@
+import json
+import hashlib
+from sseclient import SSEClient as EventSource
+import paho.mqtt.client as mqtt
+
+wiki_to_process = "itwiki"
+
+broker_host = "broker.hivemq.com"
+base_name = "pmcep/wiki"
+process_name = wiki_to_process
+
+c = mqtt.Client()
+c.connect(broker_host, 1883, 60)
+
+url = 'https://stream.wikimedia.org/v2/stream/recentchange'
+for event in EventSource(url):
+    if event.event == 'message':
+        try:
+            data = json.loads(event.data)
+            activity = data["type"]
+            case = hashlib.md5(data["title"].encode()).hexdigest()
+            process = data["wiki"]
+            if process == wiki_to_process:
+                c.publish(base_name + "/" + process_name + "/" + case + "/" + activity, "{}")
+                print(".")
+
+        except ValueError:
+            pass
